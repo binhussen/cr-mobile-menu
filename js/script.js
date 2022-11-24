cdfcb1a656f30057c26b9cffb38e2032493851b9
binhussen <mhdhsna@gmail.com>
2022-11-24 21:34:00 +0300

    Fix linter Error

--- js/script.js
+++ js/script.js
@@ -6,31 +6,31 @@ const hamburger = document.querySelector('.hamburger');
 const navItem = document.querySelectorAll('.nav-item');
 
 const openMenu = () => {
-	// logo.classList.toggle('active');
-	navMenu.classList.remove('desktop');
-	navMenu.classList.toggle('active');
-	hamburger.classList.toggle('active');
+  // logo.classList.toggle('active');
+  navMenu.classList.remove('desktop');
+  navMenu.classList.toggle('active');
+  hamburger.classList.toggle('active');
 };
 
 const closeMenu = () => {
-	navMenu.classList.add('desktop');
-	// logo.classList.remove('active');
-	navMenu.classList.remove('active');
-	hamburger.classList.remove('active');
+  navMenu.classList.add('desktop');
+  // logo.classList.remove('active');
+  navMenu.classList.remove('active');
+  hamburger.classList.remove('active');
 };
 
 hamburger.addEventListener('click', () => {
-	openMenu();
+  openMenu();
 });
 
 navItem.forEach((n) => {
-	n.addEventListener('click', () => {
-		closeMenu();
-	});
+  n.addEventListener('click', () => {
+    closeMenu();
+  });
 });
 
 document.addEventListener('keydown', (e) => {
-	if (e.key === 'Escape') {
-		closeMenu();
-	}
+  if (e.key === 'Escape') {
+    closeMenu();
+  }
 });

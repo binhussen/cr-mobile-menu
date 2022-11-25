57e63526dc1afefa39468c6e094372bae5bd57a2
binhussen <mhdhsna@gmail.com>
2022-11-25 17:04:26 +0300

    remove comment

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
-/** @format */
-
-// const logo = document.querySelector('.logo');
 const navMenu = document.querySelector('.nav-menu');
 const hamburger = document.querySelector('.hamburger');
 const navItem = document.querySelectorAll('.nav-item');
 
 const openMenu = () => {
-  // logo.classList.toggle('active');
   navMenu.classList.remove('desktop');
   navMenu.classList.toggle('active');
   hamburger.classList.toggle('active');
@@ -14,7 +10,6 @@ const openMenu = () => {
 
 const closeMenu = () => {
   navMenu.classList.add('desktop');
-  // logo.classList.remove('active');
   navMenu.classList.remove('active');
   hamburger.classList.remove('active');
 };
